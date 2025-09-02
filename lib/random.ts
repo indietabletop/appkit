629b08df296397d7f60d11193697167dbf9ca708
Artur Müller <me@arturmuller.com>
2025-09-02 15:46:59 +0200

    Add randomItemOrThrow to random module

--- lib/random.ts
+++ lib/random.ts
@@ -5,3 +5,15 @@ export function random(max: number) {
 export function randomItem<T>(array: T[]) {
   return array[random(array.length)];
 }
+
+export function randomItemOrThrow<T>(array: T[]) {
+  const item = array[random(array.length)];
+
+  if (!item) {
+    throw new Error(
+      "Could not select a random item from list. Perhaps the list is empty?",
+    );
+  }
+
+  return item;
+}
